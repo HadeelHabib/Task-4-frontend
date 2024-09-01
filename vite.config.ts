bcda4d0b6d74a429616643fecc07f22b0e63947a
hadeelhabib <hadeelhabib93@gmail.com>
2024-09-01 17:05:19 +0300

    done

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Finall_Task_Group_X4",
+  plugins: [react()],
+})
